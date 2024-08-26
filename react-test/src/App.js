9644ede2b816db0b5525d71775dd972850267e75
Your Name <643070310021@npu.ac.th>
2024-08-26 22:55:42 +0700

    add file react

---
+++ react-test/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Input from './Input';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-test/src/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Input() {
+    return (
+        <div className="Input">
+            <div className="Input__header">Creat Post</div>
+            <input 
+            className="Input__field" 
+            type="text" 
+            placeholder=""
+            />
+        </div>
+    )
+}
+export default Input;
