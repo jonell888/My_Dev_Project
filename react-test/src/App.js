d934f40a1b97624aeccdbb889d94459f4ba8212e
Your Name <643070310021@npu.ac.th>
2024-08-29 22:30:36 +0700

    Add file react

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './Navbar';
 import Input from './Input';
+import { useState } from 'react';
 
+let id = 1;
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function addPost(title) {
+    const newPost = {id, title}
+    setPosts([newPost, ...posts])
+    id += 1;
+  }
   return (
     <div className="App">
     <Navbar />
-    <Input />
+    <Input addPost={addPost}/>
     </div>
   );
 }
-
+ 
 export default App;
--- react-test/src/Input.js
+++ react-test/src/Input.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import PropTypes from  "prop-types";
 
-function Input() {
+function Input({ addPost}) {
+    const [input,setInput]= useState("");   
+
     return (
         <div className="Input">
             <div className="Input__header">Creat Post</div>
             <input 
             className="Input__field" 
-            type="text" 
+            type="text" value={input}
             placeholder=""
             />
         </div>
     )
+}
+
+Input.protoTypes = {
+    addPost: PropTypes.func.isRequired
 }
 export default Input;
